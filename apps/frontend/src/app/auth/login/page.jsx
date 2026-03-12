653e774e0f0712f07441c4287d20218e233d0b37
stugnait <i.teslitskii@gmail.com>
2026-03-13 01:01:59 +0200

    login update

--- apps/frontend/src/app/auth/login/page.jsx
+++ apps/frontend/src/app/auth/login/page.jsx
@@ -24,7 +24,7 @@ export default function LoginPage() {
 
         try {
 
-            const res = await fetch("http://localhost:5000/api/auth/login", {
+            const res = await fetch("http://seo-opt.railway.internal/api/auth/login", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json"
